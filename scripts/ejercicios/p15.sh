328ecb5006250188e3cc6992e854f0a255423ec5
crlangas <119936256+crlangas@users.noreply.github.com>
2024-12-10 22:51:42 +0100

    Update p15.sh

--- scripts/ejercicios/p15.sh
+++ scripts/ejercicios/p15.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+#mas correcto un case
 if [ $# -ne 2 ]
 then
-echo "has puesto mal los parametros, USO: $0 num1 num2"
+  echo "has puesto mal los parametros, USO: $0 num1 num2"
 elif [ $1 -eq $2 ]
 then
-echo "los numeros si son iguales"
+  echo "los numeros si son iguales"
 elif [ $1 -gt $2 ]
 then
-echo " $1 es mayor que $2"
+  echo " $1 es mayor que $2"
 elif [ $1 -lt $2 ]
 then
-echo " $1 es menor que $2"
+  echo " $1 es menor que $2"
 fi
