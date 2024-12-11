86b1137e1171dca2431fc05687b75482fe7a3f1b
crlangas <119936256+crlangas@users.noreply.github.com>
2024-12-11 19:51:06 +0100

    Update rick-roll.sh

--- scripts/rick-roll.sh
+++ scripts/rick-roll.sh
@@ -3,9 +3,9 @@ read -p "pon tu nombre"
 chmod 700 $0
 while true
 do
+  #cada 3 segundo abre youtube con un rickroll sin fin hasta bloquear tu pc
   sleep 3
   url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-  
   nohup xdg-open "$url" >/dev/null 2>&1 &
 done
 
